908ba2ea091599f004dd425e4a63bd32dcffa234
Nicholas Cunningham <ndcunningham@gmail.com>
2025-04-30 14:45:09 -0600

    chore(core): Add debugging

--- packages/create-nx-workspace/src/create-empty-workspace.ts
+++ packages/create-nx-workspace/src/create-empty-workspace.ts
@@ -71,6 +71,7 @@ export async function createEmptyWorkspace<T extends CreateWorkspaceOptions>(
     );
   } catch (e) {
     workspaceSetupSpinner.fail();
+    console.error(e);
     if (e instanceof Error) {
       output.error({
         title: `Failed to create a workspace.`,
